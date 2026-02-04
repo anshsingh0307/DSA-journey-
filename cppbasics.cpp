eba61cdf26510cad082cc750dd512f1fb3703b18
anshsingh <singhanshak57@gmail.com>
2026-02-04 21:57:35 +0530

    New to github

---
+++ cppbasics.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std ;
+int main (){
+    int x ;
+    cout<<"Enter the value of x : " ;
+    cin >> x ;
+    cout<<"The value of x is : "<< x ;
+    return 0 ; 
+}
